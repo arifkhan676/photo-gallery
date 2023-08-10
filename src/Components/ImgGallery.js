25feb6f666526bb15eb8d5634ecdc8a68d58915c
arifkhan676 <arifulhasan5500@gmail.com>
2023-08-10 23:09:33 +0600

    added redux

--- src/Components/ImgGallery.js
+++ src/Components/ImgGallery.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../App.css'
+import AddComment from './AddComment/AddComment'
 
 const ImgGallery = (props) => {
     // console.log(props.imgSelect);
@@ -8,6 +9,7 @@ const ImgGallery = (props) => {
         <div className="image-card" onClick={props.imgSelect}>
             <img className='img' src={props.img.original} alt='' />
             <button style={{ marginTop: 5 }} className='btn btn-dark'  >Review</button>
+            <AddComment imgId={props.img.imgId} />
         </div>
 
 
